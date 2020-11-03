55d8b1fcb11dfecdbcb67c7e02ffd450f841a1b3
mleila <mohamed.laila@mail.mcgill.ca>
2020-11-03 15:10:14 -0500

    NEW] add lru tests

---
+++ unitTests/test_lru_cache.py
@@ -0,0 +1,37 @@
+import unittest
+
+from lru_cache.auxiliary_structures import Node
+from lru_cache.cache import LRUCache
+
+
+class LRUTest(unittest.TestCase):
+
+    def test_lru(self):
+        '''
+        Test LRU Cache
+        '''
+        lru = LRUCache(capacity=3)
+
+        # insert all nodes
+        lru.put('1', 1)
+        lru.put('2', 2)
+        lru.put('3', 3)
+        lru.put('4', 4)
+        lru.put('5', 5)
+
+        # assert that nodes 1, and 2 should be evicted
+        self.assertFalse(lru.get('1'))
+        self.assertFalse(lru.get('2'))
+
+        # assert the the top priority node is 5
+        self.assertEqual(lru.dll.values[0], 5)
+
+        # delete a node
+        lru.delete('5')
+        self.assertEqual(lru.dll.values[0], 4)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
